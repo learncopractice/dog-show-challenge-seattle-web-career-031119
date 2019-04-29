b075e4cb3811aec9ad505d966344ab9a5f37b190
Amy Cohen <amy.cohen@flatironschool.com>
2019-04-29 13:57:27 -0700

    done with improvements

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const dogForm = document.getElementById('dog-form')
 let allDogsObj = {}
 let currentDogObj = {}
 
+//keep track of the row so it is updated in place.
+let currentRow = null
+
 
 document.addEventListener('DOMContentLoaded', () => {
     loadAllDogs()
@@ -25,15 +28,21 @@ function renderAllDogs(allDogs){
     for (let index in allDogs) {
         // console.log(allDogs[index])
         let tr = document.createElement('tr')
+        tr.setAttribute('id', allDogs[index].id)
+
         let tdName = document.createElement('td')
+        tdName.classList.add('name')
         let tdBreed = document.createElement('td')
+        tdBreed.classList.add('breed')
         let tdSex = document.createElement('td')
+        tdSex.classList.add('sex')
         let tdEdit = document.createElement('td')
         
         let editButton = document.createElement('button')
         editButton.type = 'button'
         editButton.className = 'edit'
         editButton.name = 'Edit Dog'
+        editButton.textContent = "Edit Dog"
         editButton.setAttribute('id', allDogs[index].id)
         editButton.addEventListener('click', editDog)
 
@@ -52,6 +61,16 @@ function renderAllDogs(allDogs){
 
 function editDog(event){
     event.preventDefault()
+    //find the row and keep track of it here:
+    //EXPERIMENTAL
+    // event.target.id
+    console.log("table row = " + dogTable.getElementsByTagName('tr')[event.target.id -1])
+    currentRow = dogTable.getElementsByTagName('tr')[event.target.id -1]
+    // let currentName = dogTable.getElementsByTagName('tr')[event.target.id -1].children[0].textContent
+    let currentName = currentRow.children[0].textContent
+    let currentBreed = currentRow.children[1].textContent
+    let currentSex = currentRow.children[2].textContent
+    console.log(`Name: ${currentName}, Breed: ${currentBreed}, Sex: ${currentSex}`)
 
     //finding the dog object to edit
     let dogToEdit = searchDogs(event.target.id)
@@ -68,8 +87,8 @@ function editDog(event){
 
 function searchDogs(dogId) {
     for (let index in allDogsObj) {
-        console.log(allDogsObj[index].id)
-        console.log(allDogsObj[index].id === dogId)
+        // console.log(allDogsObj[index].id)
+        // console.log(allDogsObj[index].id === dogId)
         if (allDogsObj[index].id == dogId) {
             currentDogObj = Object.assign({}, allDogsObj[index])
             return allDogsObj[index]
@@ -80,10 +99,6 @@ function searchDogs(dogId) {
 function updateDogInfo(event) {
     event.preventDefault();
 
-    // let dogToEdit = searchDogs(dogForm.getAttribute('data-dog-id'))
-    // console.log(dogToEdit)
-
-    // let EDIT_DOG_URL = `${ALL_DOGS}/${dogToEdit.id}`
     let EDIT_DOG_URL = `${ALL_DOGS}/${currentDogObj.id}`
 
     //update from form information
@@ -105,7 +120,25 @@ function updateDogInfo(event) {
         })
       })
       .then(res => res.json())
-      .then(data => renderAllDogs(data))
-      .then(document.location.reload())
+      .then(data => {
+
+        // while (dogTable.firstChild) {
+        //     dogTable.firstChild.remove()
+        // }
+        //   renderAllDogs(data)
+
+        //update the table information
+          currentRow.children[0].textContent = updateName
+          currentRow.children[1].textContent = updateBreed
+          currentRow.children[2].textContent = updateSex
+          console.log(`Name: ${currentRow.children[0].textContent}, Breed: ${currentRow.children[1].textContent}, Sex: ${currentRow.children[2].textContent}`)
+
+          //clear the form
+          currentRow = null
+          dogForm.getElementsByTagName('input')[0].value = " "
+          dogForm.getElementsByTagName('input')[1].value = " "
+          dogForm.getElementsByTagName('input')[2].value = " "      
+        })
+    //   .then(document.location.reload())
       
 }
